7ab336672b49a0c0f4be114ddcc60c8d099d3fd1
Nathan Griffith <nategri@Nathan-Personal.local>
2021-01-21 19:39:59 -0800

    alterations to do dynamic background subtraction to improve radiometry

--- hydrogen_obvs.py
+++ hydrogen_obvs.py
@@ -44,35 +44,30 @@ def retrieve_power_data():
 
     return t, avg_p
 
+def parse_output(output):
+    freqs = []
+    dbs = []
+
+    for line in power_proc_out.split('\n'):
+
+        try:
+            f, d = [float(x) for x in line.split()[:2]]
+        except:
+            continue
+
+        freqs.append(f)
+        dbs.append(d)
+
+    return freqs, dbs
+
 if __name__ == '__main__':
     # Command to enable bias-T to power LNA
     # (environment variable may not be needed in the future)
     ENABLE_BIAST_COMMAND = 'LD_LIBRARY_PATH=/usr/local/lib rtl_biast -b 1'
 
-    # Data acquisition command (right from the man page)
-    DATA_COMMAND = 'rtl_power_fftw -g 500 -f 1420405752 -B hydrogen_baseline_g500_f1420405752_t300.dat -t 300 -b 512'
-
-    #
-    # Enable bias-T
-    #
-    """
-    print("Enabling bias-T to power LNA")
-    biast_process = subprocess.Popen(
-        ENABLE_BIAST_COMMAND,
-        shell=True
-    )
-    biast_process.wait()
-    """
-
-    #
-    # Inform that a baseline needs to be taken if file does not exist
-    #
-    if os.path.isfile('hydrogen_baseline.dat'):
-        print("Baseline file found! Incorporating into output")
-    else:
-        print("Baseline file not found!")
-        print("Connect 50 Ohm terminator to LNA input and collect baseline data by running take_baseline.sh")
-        sys.exit()
+    # Data acquisition commands (right from the rtl_power_fftw man page)
+    SKY_COMMAND = 'rtl_power_fftw -d 0 -g 500 -f 1420405752 -t 300 -b 512'
+    BG_COMMAND = 'rtl_power_fftw -d 1 -g 500 -f 1420405752 -t 300 -b 512'
 
     fig, ax = pyplot.subplots(2, figsize=(8, 8))
 
@@ -82,25 +77,25 @@ if __name__ == '__main__':
     print("Running data acquisition/plotting loop...")
     while True:
         time_string = datetime.datetime.utcnow().isoformat()
-        data_process = subprocess.Popen(
-            DATA_COMMAND,
+        sky_process = subprocess.Popen(
+            SKY_COMMAND,
             shell=True,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE
         )
-        power_proc_out, _ = data_process.communicate()
+        bg_process = subprocess.Popen(
+            BG_COMMAND,
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE
+        )
+        sky_proc_out, _ = sky_process.communicate()
+        bg_proc_out, _ = bg_process.communicate()
 
-        freqs = []
-        dbs = []
-        for line in power_proc_out.split('\n'):
+        freqs, sky_dbs = parse_output(sky_proc_out)
+        _, bg_dbs = parse_output(bg_proc_out)
 
-            try:
-                f, d = [float(x) for x in line.split()[:2]]
-            except:
-                continue
-
-            freqs.append(f)
-            dbs.append(d)
+        dbs = (np.array(sky_dbs) - np.array(bg_dbs)).tolist()
 
         # Save data file
         filename = datetime.datetime.utcnow().strftime('%Y%m%d%H%M%S')
