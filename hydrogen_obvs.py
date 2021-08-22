933e06bb3cf511011fe373ed75294a583625a3a1
Nathan Griffith <nategri@Nathan-Personal.local>
2021-08-22 15:37:20 -0700

    save operating state

--- hydrogen_obvs.py
+++ hydrogen_obvs.py
@@ -5,44 +5,9 @@ import subprocess
 import datetime
 import json 
 import numpy as np
-from matplotlib import pyplot
+import time
 
-def retrieve_power_data():
-    data_dir = './data'
-    data_filenames = sorted(os.listdir(data_dir))
-
-    avg_p = []
-    t = []
-
-    for i, filename in enumerate(data_filenames):
-        with open('/'.join([data_dir, filename]), 'r') as f:
-            data = json.loads(f.read())
-
-        f = data['frequency']
-        d = data['decibels']
-        dt = dateutil.parser.parse(data['timestamp'])
-
-        f_trimmed = []
-        d_trimmed = []
-        for f, d in zip(f, d):
-            if (f > 1419.75e6) and (f<1421.25e6):
-                f_trimmed.append(f)
-                d_trimmed.append(d)
-
-        power_unitless = power_unitless_trimmed = [np.power(10.0,  x/10.0) for x in d_trimmed]
-        power_unitless_average = np.average(power_unitless_trimmed)
-        power_unitless_median = np.median (power_unitless_trimmed)
-
-        db_average = 10 * np.log10(power_unitless_average)
-        db_median = 10 * np.log10(power_unitless_median)
-
-        y = db_average
-        x = dt
-
-        avg_p.append(y)
-        t.append(dt)
-
-    return t, avg_p
+import usb
 
 def parse_output(output):
     freqs = []
@@ -61,15 +26,11 @@ def parse_output(output):
     return freqs, dbs
 
 if __name__ == '__main__':
-    # Command to enable bias-T to power LNA
-    # (environment variable may not be needed in the future)
-    ENABLE_BIAST_COMMAND = 'LD_LIBRARY_PATH=/usr/local/lib rtl_biast -b 1'
-
     # Data acquisition commands (right from the rtl_power_fftw man page)
-    SKY_COMMAND = '/home/nathan/rtl-power-fftw/build/rtl_power_fftw -d 0 -g 500 -f 1420405752 -t 300 -b 512'
-    BG_COMMAND = '/home/nathan/rtl-power-fftw/build/rtl_power_fftw -d 1 -g 500 -f 1420405752 -t 300 -b 512'
-
-    fig, ax = pyplot.subplots(2, figsize=(8, 8))
+    SKY_COMMAND = '/home/nathan/rtl-power-fftw/build/rtl_power_fftw -d 1 -g 500 -f 1420405752 -r 2000000 -t 300 -b 512'
+    BG_COMMAND = '/home/nathan/rtl-power-fftw/build/rtl_power_fftw -d 0 -g 500 -f 1420405752 -r 2000000 -t 300 -b 512'
+    #SKY_COMMAND = '/home/nathan/rtl-power-fftw/build/rtl_power_fftw -d 0 -g 500 -f 1415000000 -r 2500000 -t 600 -b 512'
+    #BG_COMMAND = '/home/nathan/rtl-power-fftw/build/rtl_power_fftw -d 1 -g 500 -f 1415000000 -r 2500000 -t 600 -b 512'
 
     #
     # Repeatedly collect and plot power data
@@ -81,16 +42,46 @@ if __name__ == '__main__':
             SKY_COMMAND,
             shell=True,
             stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE
+            stderr=subprocess.PIPE,
+            encoding='utf8'
         )
         bg_process = subprocess.Popen(
             BG_COMMAND,
             shell=True,
             stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE
+            stderr=subprocess.PIPE,
+            encoding='utf8'
         )
-        sky_proc_out, _ = sky_process.communicate()
-        bg_proc_out, _ = bg_process.communicate()
+
+        while True:
+            try:
+                sky_proc_out, _ = sky_process.communicate(timeout=600)
+                bg_proc_out, _ = bg_process.communicate(timeout=600)
+                break
+            except subprocess.TimeoutExpired:
+                print("Timeout! Resetting SDR hardware...")
+                sky_process.kill()
+                bg_process.kill()
+
+                for usb_sdr in usb.core.find(idVendor=0x0bda, idProduct=0x2838, find_all=True):
+                    usb_sdr.reset()
+
+                time.sleep(5)
+
+                sky_process = subprocess.Popen(
+                    SKY_COMMAND,
+                    shell=True,
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.PIPE,
+                    encoding='utf8'
+                )
+                bg_process = subprocess.Popen(
+                    BG_COMMAND,
+                    shell=True,
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.PIPE,
+                    encoding='utf8'
+                )
 
         freqs, sky_dbs = parse_output(sky_proc_out)
         _, bg_dbs = parse_output(bg_proc_out)
@@ -108,33 +99,5 @@ if __name__ == '__main__':
             }
 
             f.write(json.dumps(data))
-
-        freqs = np.array(freqs) / 1.0e6
-
-        dbs_std = np.std(dbs)
-        dbs_median = np.median(dbs)
-
-        x_pow, y_pow = retrieve_power_data()
-
-        ax[0].clear()
-        ax[1].clear()
-
-        ax[0].plot(freqs, dbs, '-b')
-        ax[0].set_ylim(dbs_median - 0.2, dbs_median + 1.5)
-        ax[0].set_xlabel('MHz')
-        ax[0].set_ylabel('dB / Hz')
-        ax[0].tick_params(labelsize=8)
-
-        ax[1].plot(x_pow, y_pow, '*b')
-        ax[1].set_xlabel('Date')
-        ax[1].set_ylabel('Power')
-        for tick_label in ax[1].get_xticklabels():
-            tick_label.set_ha("right")
-            tick_label.set_rotation(30)
-
-        pyplot.tight_layout()
-
-        pyplot.pause(0.5)
-        pyplot.draw()
 
         print(datetime.datetime.utcnow())
--- render_analysis.py
+++ render_analysis.py
@@ -35,20 +35,16 @@ class TelescopeData:
 
         for data in data_list:
             f = data['frequency']
-            d = data['decibels']
+            d = -1*np.array(data['decibels']) # Multiply by -1 if background and signal are swapped in data files
             dt = dateutil.parser.parse(data['timestamp'])
 
             f_lower = f[0] + 2.0e5
             f_upper = f[-1] - 2.0e5
 
-            f_trimmed = []
-            d_trimmed = []
-            for f, d in zip(f, d):
-                if (f > f_lower) and (f < f_upper):
-                    f_trimmed.append(f)
-                    d_trimmed.append(d)
+            f_trimmed = [x for x, y in zip(f, d) if x > f_lower and x < f_upper]
+            d_trimmed = [y for x, y in zip(f, d) if x > f_lower and x < f_upper]
 
-            power_unitless = power_unitless_trimmed = [np.power(10.0,  x/10.0) for x in d_trimmed]
+            power_unitless_trimmed = [np.power(10.0,  x/10.0) for x in d_trimmed]
             power_unitless_agg = agg_func(power_unitless_trimmed)
 
             p = 10 * np.log10(power_unitless_agg)
@@ -73,6 +69,9 @@ class DataRenderer:
         self._fig, self._ax = pyplot.subplots(nrows=2, ncols=2, figsize=(16, 8))
         self._sky_data = np.loadtxt('neutral_hydrogen_sky.dat')
         self._sky_data = np.flip(self._sky_data, 1)
+
+        self._ax[0][1].imshow(self._sky_data, extent=[24, 0, -90, 90], aspect='auto', interpolation='gaussian')
+        self._telescope_position_scatter = None
 
     def _azel_to_radec(self, az, el, date_string):
         # Credit to: https://github.com/0xCoto/Virgo
@@ -113,16 +112,19 @@ class DataRenderer:
 
         # Clear plot
         self._ax[0][0].clear()
-        self._ax[0][1].clear()
+        #self._ax[0][1].clear()
         self._ax[1][0].clear()
         self._ax[1][1].clear()
+
+        if self._telescope_position_scatter is not None:
+            self._telescope_position_scatter.remove()
 
         self._fig.suptitle(current_date_string[:-5])
 
         # Spectrum plot
         freqs = current_freq_data['frequency']
-        dbs = current_freq_data['decibels']
-        dbs_median = np.median(current_freq_data['decibels'])
+        dbs = -1*np.array(current_freq_data['decibels'])
+        dbs_median = np.median(dbs)
         self._ax[0][0].plot(np.array(freqs) / 1.0e6, dbs, '-b', label='Spectrum')
         self._ax[0][0].set_ylim(dbs_median - 0.2, dbs_median + 1.5)
         self._ax[0][0].set_xlabel('MHz')
@@ -132,7 +134,7 @@ class DataRenderer:
         self._ax[0][0].legend(loc='upper left')
 
         # Waterfall plot
-        waterfall_data = [x['decibels'] for x in freq_data][::-1]
+        waterfall_data = [-1*np.array(x['decibels']) for x in freq_data][::-1]
         if len(waterfall_data) < DAY_OF_TIMESTEPS:
             diff = DAY_OF_TIMESTEPS - len(waterfall_data)
             pad = [len(waterfall_data[0])*[-10.0] for _ in range(diff)]
@@ -143,16 +145,15 @@ class DataRenderer:
         self._ax[1][0].get_yaxis().set_ticks([])
         self._ax[1][0].set_ylabel('Previous 24 Hours')
         self._ax[1][0].tick_params(labelsize=8)
-        self._ax[1][0].imshow(waterfall_data, extent=[bottom_freq, top_freq, 0.0, 1.0], cmap='jet', aspect='auto', vmin=-6.6, vmax=-5.5)
+        self._ax[1][0].imshow(waterfall_data, extent=[bottom_freq, top_freq, 0.0, 1.0], cmap='jet', aspect='auto', vmin=-4.3, vmax=-3.5)
 
         # Map plot
         # Credit to: https://github.com/0xCoto/Virgo
         ra, dec = self._azel_to_radec(azimuth, elevation, current_date_string)
-        self._ax[0][1].imshow(self._sky_data, extent=[24, 0, -90, 90], aspect='auto', interpolation='gaussian')
         self._ax[0][1].set_xlabel('Right Ascension')
         self._ax[0][1].set_ylabel('Declination')
         self._ax[0][1].tick_params(labelsize=8)
-        self._ax[0][1].scatter(ra, dec, s=200, color=[0.85, 0.15, 0.16], label='Telescope')
+        self._telescope_position_scatter = self._ax[0][1].scatter(ra, dec, s=200, color=[0.85, 0.15, 0.16], label='Telescope')
         self._ax[0][1].legend(loc='upper left')
 
         # Power plot
@@ -160,7 +161,8 @@ class DataRenderer:
         self._ax[1][1].set_xlabel('Previous 12 Hours')
         self._ax[1][1].set_ylabel('dB')
         self._ax[1][1].set_xlim(power_data[0][-1] - datetime.timedelta(days=0.5), power_data[0][-1])
-        self._ax[1][1].set_ylim(-6.5, -6.2)
+        #self._ax[1][1].set_ylim(-4.2, -3.8)
+        self._ax[1][1].set_ylim(-4.5, -3.2)
         self._ax[1][1].tick_params(labelsize=8)
         for tick_label in self._ax[1][1].get_xticklabels():
             tick_label.set_ha("right")
@@ -207,8 +209,9 @@ if __name__ == "__main__":
     
     data_renderer = DataRenderer(args.data)
 
-    fileames = sorted(os.listdir(args.data))
+    filenames = sorted(os.listdir(args.data))[int(-1.5*288):]
 
-    for i, fn in enumerate(fileames):
-        print("{} of {}".format(i, len(fileames)))
+    for i, fn in enumerate(filenames):
+        print("{} of {}".format(i, len(filenames)))
+        print(fn)
         data_renderer.render(fn, (args.az, args.el), image='render_output/'+str(i).zfill(4))
